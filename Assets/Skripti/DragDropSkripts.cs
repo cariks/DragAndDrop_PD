bd8e4aad64f0aec290df769dca5ce2b2303e1788
cariks <eriks.cariks.lvt@gmail.com>
2023-06-09 18:42:46 +0300

    Projektam strādā visas darbības, izlabotas kļudas

--- Assets/Skripti/DragDropSkripts.cs
+++ Assets/Skripti/DragDropSkripts.cs
@@ -1,60 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//Jāieimportē, lai varētu lietot visus I interfeisus
-using UnityEngine.EventSystems;
+using UnityEngine.EventSystems; 	//Jāieimportē, lai varētu lietot visus I interfeisus
 
 public class DragDropSkripts : MonoBehaviour, 
     IPointerDownHandler, IBeginDragHandler, 
-    IDragHandler, IEndDragHandler {
-    //Uzglabā norādi uz Objekti skriptu
-    public Objekti objektuSkripts;
-    //Velkamam objektam piestiprinātā CanvasGoup komponente
-    private CanvasGroup kanvasGrupa;
-    //Objekta atrašanās vieta, kurš tiek pārvietots
+    IDragHandler, IEndDragHandler { 	//Uzglabā norādi uz Objekti skriptu
+    public Objekti objektuSkripts;		//Velkamam objektam piestiprinātā CanvasGoup komponente
+    private CanvasGroup kanvasGrupa;	//Objekta atrašanās vieta, kurš tiek pārvietots
     private RectTransform velkObjRectTransf;
 
     void Start()
     {
-        //Piekļūst objektam piestiprinātajai CanvasGroup
-        //komponentei
-        kanvasGrupa = GetComponent<CanvasGroup>();
-        //Piekļūst objeta RectTransform komponentei
-        velkObjRectTransf = GetComponent<RectTransform>();
+		kanvasGrupa = GetComponent<CanvasGroup>();	//Piekļūst objektam piestiprinātajai CanvasGroup komponentei
+		velkObjRectTransf = GetComponent<RectTransform>();	//Piekļūst objeta rectTransform komponentei
     }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        Debug.Log("Uzklikšķināts uz velkama objekta!");
-        objektuSkripts.pedejaisVilktais = null;
-        kanvasGrupa.alpha = 0.6f;
-        kanvasGrupa.blocksRaycasts = false;
+		Debug.Log("Uzklikšķināts uz velkama objekta!");
+		objektuSkripts.pedejaisVilktais = null;		// Notiek vilktā objekta norādes atiestatīšana
+		kanvasGrupa.alpha = 0.6f;		// Iestatām vilktā objekta CanvasGroup alfa vērtību
+		kanvasGrupa.blocksRaycasts = false;		// Iestatām vilktā objekta CanvasGroup "blocksRaycasts" vērtību
     }
 
-    //Turpināsim no šīs vietas
+
     public void OnDrag(PointerEventData eventData)
     {
-        velkObjRectTransf.anchoredPosition +=
-        eventData.delta / objektuSkripts.kanva.scaleFactor; 
+		velkObjRectTransf.anchoredPosition +=
+			eventData.delta / objektuSkripts.kanva.scaleFactor; 	// Pārvietojam vilkto objektu, pamatojoties uz peles kustības datiem
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        objektuSkripts.pedejaisVilktais =
-             eventData.pointerDrag;
-        kanvasGrupa.alpha = 1f;
+        objektuSkripts.pedejaisVilktais = eventData.pointerDrag;	// Saglabājam pedējo vilkto objektu
+		kanvasGrupa.alpha = 1f;		// Atjaunojam vilkāmā objekta CanvasGroup alfa vērtību
 
-        if(objektuSkripts.vaiIstajaVieta == false) {
-            kanvasGrupa.blocksRaycasts = true;
+		if(objektuSkripts.vaiIstajaVieta == false) {	// Pārbaudām, vai objekts ir atpakaļ savā vietā
+			kanvasGrupa.blocksRaycasts = true;		// Iestatām vilktā objekta CanvasGroup "blocksRaycasts" vērtību
 
         } else {
-            objektuSkripts.pedejaisVilktais = null;
+			objektuSkripts.pedejaisVilktais = null;		// Notiek vilktā objekta norādes atiestatīšana
         }
 
-        objektuSkripts.vaiIstajaVieta = false;
+		objektuSkripts.vaiIstajaVieta = false;		// Atjaunojam objekta "vaiIstajaVieta" vērtību
     }
 
-    public void OnPointerDown(PointerEventData eventData)
+	public void OnPointerDown(PointerEventData eventData)
     {
         
     }
--- Assets/Skripti/Laiks.cs
+++ Assets/Skripti/Laiks.cs
@@ -1,38 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using System.Runtime.InteropServices;
+using UnityEngine.UI;
 
 public class Laiks : MonoBehaviour {
-	public static float timerStart;
-	public GameObject textBox;
 
-	private static string t;
+	public GameObject textBox;	// GameObject, kas satur teksta lauku
+	public static float timerStart;
+	private static string timer_text;	// Laika rādītāja teksts
 
 
 	// Use this for initialization
 	void Start () {
-		timerStart = 0;
-		t = getTime(false);
-		textBox.GetComponent<Text>().text = t;
+		timerStart = 0;		// Sākuma taimera vērtība ir 0
+		timer_text = getTime(false);	// Iegūstam sākotnējo laiku
+		textBox.GetComponent<Text>().text = timer_text;		// Iestatām teksta laukā rādāmo laiku
 	}
 		
 
 	public static string getTime(bool Seconds) {
-		int h, m, s;
-		s = Mathf.RoundToInt(timerStart);
+		int h, m, s;	// Mainīgie laika vienībam
+		string a, b, c;		// Mainīgie laika attēlošanai
+
+		s = Mathf.RoundToInt(timerStart);	// Noapaļojam taimeru
 		if (Seconds)
 		{
-			return ""+s;
+			return ""+s;	// Ja ir pieprasīti tikai sekundes, tad atgriežam tikai sekundes vērtību
 		}
 
 		h = s/3600;
 		s -= h*3600;
 		m = s/60;
 		s -= m*60;
-
-		string a, b, c;
 
 		if (s < 10) {
 			c = ":0" + s;
@@ -52,18 +52,17 @@ public class Laiks : MonoBehaviour {
 			a = "" + h;
 		}
 
-		t = a+b+c;
+		timer_text = a+b+c;
 
-		return t;
+		return timer_text;
 	}
-
 
 
 	// Update is called once per frame
 	void Update () {
 		timerStart += Time.deltaTime;
-		t = getTime (false);
+		timer_text = getTime (false);
 
-		textBox.GetComponent<Text> ().text = t;
+		textBox.GetComponent<Text> ().text = timer_text;
 	}
 }
